5a717c5e7d8fe39cc1962bf79c7807ab58395883
Marek Potociar <marek.potociar@gmail.com>
2016-12-13 10:43:24 +0100

    Added support for building images behind proxy.
    Signed-off-by: Marek Potociar <marek.potociar@gmail.com>

--- agent/build-docker.sh
+++ agent/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t code-tv/code-tv-agent .
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t code-tv/code-tv-agent .
--- master/build-docker.sh
+++ master/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t code-tv/code-tv-master .
+docker build --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy -t code-tv/code-tv-master .
